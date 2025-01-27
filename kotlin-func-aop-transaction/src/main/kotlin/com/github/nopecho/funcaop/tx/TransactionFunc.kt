87c0a44f61d26039609922040e018e19187d4660
nopecho <ch.jooon@gmail.com>
2025-01-27 22:50:32 +0900

    feat: add spring functional transaction advice

---
+++ kotlin-func-aop-transaction/src/main/kotlin/com/github/nopecho/funcaop/tx/TransactionFunc.kt
@@ -0,0 +1,32 @@
+package com.github.nopecho.funcaop.tx
+
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+
+
+@Component
+class TransactionFunc(private val _advice: TransactionAdvice) {
+
+    companion object {
+        private lateinit var advice: TransactionAdvice
+
+        fun <T> execute(readOnly: Boolean = false, block: () -> T): T = when {
+            readOnly -> advice.readOnly(block)
+            else -> advice.transactional(block)
+        }
+    }
+
+    init {
+        advice = _advice
+    }
+
+    @Component
+    class TransactionAdvice {
+
+        @Transactional
+        fun <T> transactional(block: () -> T): T = block()
+
+        @Transactional(readOnly = true)
+        fun <T> readOnly(block: () -> T): T = block()
+    }
+}
